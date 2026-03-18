e77fad9d34cc0211aa0ca20d82de84b740037c43
Qiyu Li <qil047@ucsd.edu>
2026-03-18 12:58:45 +0000

    fix: conditional before_repo_cmd; add [build failed] parser detection

--- .swebench/entrypoint.sh
+++ .swebench/entrypoint.sh
@@ -13,8 +13,11 @@ git checkout e658995760ac1209cb12df97027a2e282b4536ae
 # Apply user patch
 git apply -v /workspace/patch.diff || echo 'WARNING: patch apply failed'
 
-# Apply test setup (mirrors before_repo_set_cmd)
-git checkout 379058e10f3dbc0fdcaf80394bd09b18927e7d33 -- test/integration/targets/ansible-doc/broken-docs/collections/ansible_collections/testns/testcol/plugins/lookup/noop.py test/lib/ansible_test/_util/controller/sanity/pylint/plugins/unwanted.py test/support/integration/plugins/module_utils/network/common/utils.py test/support/network-integration/collections/ansible_collections/ansible/netcommon/plugins/module_utils/network/common/utils.py test/units/executor/module_common/test_recursive_finder.py test/units/module_utils/common/test_collections.py test/units/module_utils/conftest.py test/units/modules/conftest.py
+# Apply test setup only when a real code patch was provided (not just .swebench/.github)
+HAS_CODE_PATCH=$(grep "^diff --git" /workspace/patch.diff 2>/dev/null | grep -v "\.swebench\|\.github\|submit\.sh\|\.gitignore" | wc -l)
+if [ "$HAS_CODE_PATCH" -gt 0 ]; then
+  git checkout 379058e10f3dbc0fdcaf80394bd09b18927e7d33 -- test/integration/targets/ansible-doc/broken-docs/collections/ansible_collections/testns/testcol/plugins/lookup/noop.py test/lib/ansible_test/_util/controller/sanity/pylint/plugins/unwanted.py test/support/integration/plugins/module_utils/network/common/utils.py test/support/network-integration/collections/ansible_collections/ansible/netcommon/plugins/module_utils/network/common/utils.py test/units/executor/module_common/test_recursive_finder.py test/units/module_utils/common/test_collections.py test/units/module_utils/conftest.py test/units/modules/conftest.py
+fi
 
 # Run tests
 bash /workspace/run_script.sh test/lib/ansible_test/_util/controller/sanity/pylint/plugins/unwanted.py,test/units/modules/conftest.py,test/units/module_utils/conftest.py,test/integration/targets/ansible-doc/broken-docs/collections/ansible_collections/testns/testcol/plugins/lookup/noop.py,test/units/executor/module_common/test_recursive_finder.py,test/support/network-integration/collections/ansible_collections/ansible/netcommon/plugins/module_utils/network/common/utils.py,test/support/integration/plugins/module_utils/network/common/utils.py,test/units/module_utils/common/test_collections.py > /workspace/stdout.log 2> /workspace/stderr.log
